930abbd47747aae989981ba659b7d027d14df136
Zaid Nolley <nolle044@umn.edu>
2024-05-11 17:47:39 -0500

    initial commit

---
+++ content.js
@@ -0,0 +1,16 @@
+/*window.onload = function() {
+    let imgReplace = document.getElementsByClassName("r-4qtqp9 r-yyyyoo r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-lrsllp r-1nao33i r-16y2uox r-8kz0gk");
+    console.log(imgReplace);
+    console.log(imgReplace.item(0));
+    imgReplace[0].outerHTML = '<svg viewBox="0 0 48 24" aria-hidden="true" class="r-4qtqp9 r-yyyyoo r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-lrsllp r-1nao33i r-16y2uox r-8kz0gk"><g><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"></path></g></svg>'
+}*/
+
+
+setTimeout(() => {
+    let imgReplace = document.getElementsByClassName("r-4qtqp9 r-yyyyoo r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-lrsllp r-1nao33i r-16y2uox r-8kz0gk");
+    console.log(imgReplace);
+    console.log(imgReplace.item(0));
+    //imgReplace[0].outerHTML = '<svg viewBox="0 0 48 24" aria-hidden="true" class="r-4qtqp9 r-yyyyoo r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-lrsllp r-1nao33i r-16y2uox r-8kz0gk"><g><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"></path></g></svg>'
+    imgReplace[0].outerHTML = '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Logo_of_Twitter.svg/2491px-Logo_of_Twitter.svg.png" width="48" height="43"</img>'
+
+  }, 1000);
